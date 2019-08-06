d37cf933cb15efc34a54f3ee1604754797d4347f
kaue0101 <53578481+kaue0101@users.noreply.github.com>
2019-08-06 16:13:58 -0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,15 @@
+ls - listagem dos comandos q possui  no linux
+
+lh no modo hmano convertendo em mb gb hrs
+
+CD/ se encontra na raiz
+
+COR AZUL DIRETORIO  AZUL CLARO LINKS ATALHO SIMBOLICO  CINZA ARQUIVOS MARCATESTO VERDE PERIGO 
+
+ pwd ferifica qual diretorio esta
+
+mkdir linuxmint sequencia lsls
+
+COMANDOS DE ISTALAÇÃ: sudo apt update && sudo apt istall git
+
+GIT CLONE trazer arquivos do git e clonar
